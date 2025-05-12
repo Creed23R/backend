3564dea250f09a85041fc81eb441359070fab1fb
Creed23R <kevinriosnicho@gmail.com>
2025-05-12 14:05:47 -0500

    feat: add CORS support for production environment

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:8081'], // Permite solo solicitudes desde localhost:3000
+    origin: ['http://localhost:3000', 'http://localhost:8081', 'https://e-commerce-3n9r.vercel.app'], // Permite solo solicitudes desde localhost:3000
     methods: 'GET,POST,PUT,DELETE,PATCH', // Permite solo ciertos métodos
     allowedHeaders: 'Content-Type, Accept', // Permite solo ciertos encabezados
   });
